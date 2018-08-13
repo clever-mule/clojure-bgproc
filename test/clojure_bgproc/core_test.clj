592d288dc54bdc59eae3cebdcfd7076bab0bceb0
Artemiy Solopov <art-solopov@yandex.ru>
2018-08-14 00:11:17 +0300

    Initial commit

---
+++ test/clojure_bgproc/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-bgproc.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-bgproc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
